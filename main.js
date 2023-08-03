8d52cee68d40397e82a30652fad38a4c1f250a96
Jasveer04 <117168315+Jasveer04@users.noreply.github.com>
2023-08-03 16:50:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+x = 0;
+y = 0;
+draw_circle = "";
+draw_rect = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("status").innerHTML = "System Is Listening Please Speak";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+
+    var content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The Speech Has Been Recognised As: "+ content;
+    if(content =="circle")
+    {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started Drawing Circle";
+        draw_circle = "set";
+    }
+    if(content =="rectangle")
+    {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started Drawing Rectangle";
+        draw_rect = "set";
+    }
+    
+    if(content =="circle rectangle")
+    {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started Drawing Rectangle";
+        draw_rect = "set";
+        
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started Drawing Circle";
+        draw_circle = "set";
+    }
+
+    if(content =="rectangle circle")
+    {
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started Drawing Rectangle";
+        draw_rect = "set";
+        
+        x = Math.floor(Math.random() * 900);
+        y = Math.floor(Math.random() * 600);
+        document.getElementById("status").innerHTML = "Started Drawing Circle";
+        draw_circle = "set";
+    }
+
+    if(content =="clear"){
+    clear();
+    document.getElementById("status").innerHTML = "Canvas Cleared";
+    }
+}
+
+function draw() {
+
+    if(draw_circle == "set")
+    {
+        radius = Math.floor(Math.random() * 100);
+        circle(x,y,radius);
+        document.getElementById("status").innerHTML = "Circle Is Drawn. ";
+        draw_circle = "";
+    } 
+
+    if(draw_rect == "set")
+    {
+        rect(x,y,70,50);
+        document.getElementById("status").innerHTML = "Rectangle Is Drawn";
+        draw_rect = "";
+    }
+}
+
+function setup() {
+    canvas = createCanvas(900, 600)
+}
